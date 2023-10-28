90e9a2ea0444b7e2008eab0677eb18d173d60f2c
Katherine <katherine@Liyas-7.crimson.local>
2023-10-28 15:05:06 -0400

    Submit.

--- typescript/test.js
+++ typescript/test.js
@@ -1,6 +1,15 @@
-function square(number) {
-    return number * number;
+function square(n) {
+    return n * n;
 }
 function numberString(i) {
-    return "";
+    var s = "";
+    if (i <= 0) {
+        return "-1";
+    }
+    else {
+        for (var j = 1; j <= i; j++) {
+            s += j;
+        }
+    }
+    return s;
 }
